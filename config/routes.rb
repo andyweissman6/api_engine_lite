e4ee8463d3e73e87c6fa05dbcdaa467df59a3430
Andy Weissman <andyweissman6@gmail.com>
2023-07-11 19:16:49 -0600

    list of merchants; single merchant; merchant items (unfinished)
    forgot to commit for some time: write tests, create controllers, create serializers,
    postman tests pass for list of/single merchant. Still working on merchant items postman tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  namespace :api do
+    namespace :v1 do
+      resources :merchants, only: [:index, :show] do
+        resources :items, only: [:index], module: :merchants
+      end
+    end
+  end
 end
---
+++ spec/requests/api/v1/merchant_items_request_spec.rb
@@ -0,0 +1,39 @@
+require "rails_helper"
+
+describe "Merchant Items API" do
+  it "sends a list of merchants' items" do
+    merchant_1 = Merchant.create!(name: "Paula Pounders")
+    item1 = Item.create!( name: "Shampoo",
+                          description: "Shampoo is better!",
+                          unit_price: 4.20,
+                          merchant_id: merchant_1.id  )
+    item2 = Item.create!( name: "Conditioner",
+                          description: "I make hair silky and smooth!",
+                          unit_price: 3.99,
+                          merchant_id: merchant_1.id  )
+    item3 = Item.create!( name: "Blue Duck",
+                          description: "I drew the duck blue because I've never seen a blue duck before, and to be honest with you, I wanted to see a blue duck.",
+                          unit_price: 100.25,
+                          merchant_id: merchant_1.id  )
+    
+    get "/api/v1/merchants/#{merchant_1.id}/items"
+
+    expect(response).to be_successful
+
+    merchant_items = JSON.parse(response.body, symbolize_names: true)
+    
+    merchant_items[:data].each do |item|
+      expect(item).to have_key(:id)
+      expect(item[:id].to_i).to be_an(Integer)
+
+      expect(item).to have_key(:type)
+      expect(item[:type]).to be_a(String)
+
+      expect(item).to have_key(:attributes)
+      expect(item[:attributes][:name]).to be_a(String)
+      expect(item[:attributes][:description]).to be_a(String)
+      expect(item[:attributes][:unit_price]).to be_a(Float)
+      expect(item[:attributes][:merchant_id]).to eq(merchant_1.id)
+    end
+  end
+end
